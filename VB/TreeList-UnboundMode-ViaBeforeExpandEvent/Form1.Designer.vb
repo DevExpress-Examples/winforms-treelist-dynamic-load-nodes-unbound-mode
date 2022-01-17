d1eb32561f7f657f03ed53572690d1175c2cfbab
DevExpressExampleBot <fakeEmail>
2022-01-17 19:06:03 +0300

    Source auto update [skip ci]

--- VB/TreeList-UnboundMode-ViaBeforeExpandEvent/Form1.Designer.vb
+++ VB/TreeList-UnboundMode-ViaBeforeExpandEvent/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace TreeList_UnboundMode_ViaBeforeExpandEvent
-    Partial Public Class Form1
+Namespace TreeList_UnboundMode_ViaBeforeExpandEvent
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,24 +12,24 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
             Me.components = New System.ComponentModel.Container()
-            Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TreeList_UnboundMode_ViaBeforeExpandEvent.Form1))
             Me.treeList1 = New DevExpress.XtraTreeList.TreeList()
             Me.imageList1 = New System.Windows.Forms.ImageList(Me.components)
-            DirectCast(Me.treeList1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.treeList1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' treeList1
@@ -40,7 +42,7 @@
             ' 
             ' imageList1
             ' 
-            Me.imageList1.ImageStream = (DirectCast(resources.GetObject("imageList1.ImageStream"), System.Windows.Forms.ImageListStreamer))
+            Me.imageList1.ImageStream = CType((resources.GetObject("imageList1.ImageStream")), System.Windows.Forms.ImageListStreamer)
             Me.imageList1.TransparentColor = System.Drawing.Color.Magenta
             Me.imageList1.Images.SetKeyName(0, "")
             Me.imageList1.Images.SetKeyName(1, "")
@@ -54,15 +56,13 @@
             Me.Controls.Add(Me.treeList1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(Me.treeList1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.treeList1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+#End Region
         Private treeList1 As DevExpress.XtraTreeList.TreeList
+
         Private imageList1 As System.Windows.Forms.ImageList
     End Class
 End Namespace
-
--- VB/TreeList-UnboundMode-ViaBeforeExpandEvent/Form1.vb
+++ VB/TreeList-UnboundMode-ViaBeforeExpandEvent/Form1.vb
@@ -1,18 +1,13 @@
-﻿Imports DevExpress.XtraTreeList.Columns
+Imports DevExpress.XtraTreeList.Columns
 Imports DevExpress.XtraTreeList.Nodes
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
 Imports System.IO
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows.Forms
 
 Namespace TreeList_UnboundMode_ViaBeforeExpandEvent
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
@@ -22,11 +17,10 @@ Namespace TreeList_UnboundMode_ViaBeforeExpandEvent
         End Sub
 
         Private Sub InitTreeList()
-            Dim treeListColumn1 As New TreeListColumn()
-            Dim treeListColumn2 As New TreeListColumn()
-            Dim treeListColumn3 As New TreeListColumn()
-            Dim treeListColumn4 As New TreeListColumn()
-
+            Dim treeListColumn1 As TreeListColumn = New TreeListColumn()
+            Dim treeListColumn2 As TreeListColumn = New TreeListColumn()
+            Dim treeListColumn3 As TreeListColumn = New TreeListColumn()
+            Dim treeListColumn4 As TreeListColumn = New TreeListColumn()
             ' 
             ' treeListColumn1
             ' 
@@ -58,11 +52,8 @@ Namespace TreeList_UnboundMode_ViaBeforeExpandEvent
             treeListColumn4.Format.FormatString = "n0"
             treeListColumn4.VisibleIndex = 2
             treeListColumn4.Width = 123
-
-
-            treeList1.Columns.AddRange(New DevExpress.XtraTreeList.Columns.TreeListColumn() { treeListColumn1, treeListColumn2, treeListColumn3, treeListColumn4})
-
-            treeList1.Dock = System.Windows.Forms.DockStyle.Fill
+            treeList1.Columns.AddRange(New TreeListColumn() {treeListColumn1, treeListColumn2, treeListColumn3, treeListColumn4})
+            treeList1.Dock = DockStyle.Fill
             treeList1.OptionsBehavior.AutoChangeParent = False
             treeList1.OptionsBehavior.AutoNodeHeight = False
             treeList1.OptionsBehavior.CloseEditorOnLostFocus = False
@@ -70,12 +61,10 @@ Namespace TreeList_UnboundMode_ViaBeforeExpandEvent
             treeList1.OptionsSelection.KeepSelectedOnClick = False
             treeList1.OptionsBehavior.ShowToolTips = False
             treeList1.OptionsBehavior.SmartMouseHover = False
-
-            treeList1.StateImageList = Me.imageList1
-
-            AddHandler treeList1.AfterCollapse, AddressOf treeList1_AfterCollapse
-            AddHandler treeList1.AfterExpand, AddressOf treeList1_AfterExpand
-            AddHandler treeList1.BeforeExpand, AddressOf treeList1_BeforeExpand
+            treeList1.StateImageList = imageList1
+            AddHandler treeList1.AfterCollapse, New DevExpress.XtraTreeList.NodeEventHandler(AddressOf treeList1_AfterCollapse)
+            AddHandler treeList1.AfterExpand, New DevExpress.XtraTreeList.NodeEventHandler(AddressOf treeList1_AfterExpand)
+            AddHandler treeList1.BeforeExpand, New DevExpress.XtraTreeList.BeforeExpandEventHandler(AddressOf treeList1_BeforeExpand)
         End Sub
 
         Private Sub InitData()
@@ -87,21 +76,20 @@ Namespace TreeList_UnboundMode_ViaBeforeExpandEvent
             Dim node As TreeListNode
             Dim di As DirectoryInfo
             Try
-                Dim root() As String = Directory.GetDirectories(path)
+                Dim root As String() = Directory.GetDirectories(path)
                 For Each s As String In root
                     Try
                         di = New DirectoryInfo(s)
-                        node = treeList1.AppendNode(New Object() { s, di.Name, "Folder", Nothing }, pNode)
+                        node = treeList1.AppendNode(New Object() {s, di.Name, "Folder", Nothing}, pNode)
                         node.StateImageIndex = 0
                         node.HasChildren = HasFiles(s)
-                        If node.HasChildren Then
-                            node.Tag = True
-                        End If
+                        If node.HasChildren Then node.Tag = True
                     Catch
                     End Try
-                Next s
+                Next
             Catch
             End Try
+
             InitFiles(path, pNode)
             treeList1.EndUnboundLoad()
         End Sub
@@ -110,26 +98,22 @@ Namespace TreeList_UnboundMode_ViaBeforeExpandEvent
             Dim node As TreeListNode
             Dim fi As FileInfo
             Try
-                Dim root() As String = Directory.GetFiles(path)
+                Dim root As String() = Directory.GetFiles(path)
                 For Each s As String In root
                     fi = New FileInfo(s)
-                    node = treeList1.AppendNode(New Object() { s, fi.Name, "File", fi.Length }, pNode)
+                    node = treeList1.AppendNode(New Object() {s, fi.Name, "File", fi.Length}, pNode)
                     node.StateImageIndex = 1
                     node.HasChildren = False
-                Next s
+                Next
             Catch
             End Try
         End Sub
 
         Private Function HasFiles(ByVal path As String) As Boolean
-            Dim root() As String = Directory.GetFiles(path)
-            If root.Length > 0 Then
-                Return True
-            End If
+            Dim root As String() = Directory.GetFiles(path)
+            If root.Length > 0 Then Return True
             root = Directory.GetDirectories(path)
-            If root.Length > 0 Then
-                Return True
-            End If
+            If root.Length > 0 Then Return True
             Return False
         End Function
 
@@ -144,16 +128,11 @@ Namespace TreeList_UnboundMode_ViaBeforeExpandEvent
         End Sub
 
         Private Sub treeList1_AfterExpand(ByVal sender As Object, ByVal e As DevExpress.XtraTreeList.NodeEventArgs)
-            If e.Node.StateImageIndex <> 1 Then
-                e.Node.StateImageIndex = 2
-            End If
+            If e.Node.StateImageIndex <> 1 Then e.Node.StateImageIndex = 2
         End Sub
 
         Private Sub treeList1_AfterCollapse(ByVal sender As Object, ByVal e As DevExpress.XtraTreeList.NodeEventArgs)
-            If e.Node.StateImageIndex <> 1 Then
-                e.Node.StateImageIndex = 0
-            End If
+            If e.Node.StateImageIndex <> 1 Then e.Node.StateImageIndex = 0
         End Sub
     End Class
-
 End Namespace
